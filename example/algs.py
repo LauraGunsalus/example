b2dac0b0375d9d77272c91addd1c7b14c11d4454
lgunsalus <lgunsalus@kepler.local>
2019-01-25 18:32:11 -0800

    updated algs

--- example/algs.py
+++ example/algs.py
@@ -8,18 +8,55 @@ def pointless_sort(x):
     return np.array([1,2,3])
 
 def bubblesort(x):
-    """
-    Describe how you are sorting `x`
-    """
+    assignments = 0
+    conditionals = 0
+    for i in range(len(x)):
+        assignments += 1
+        for j in range(1, len(x)):
+            assignments += 1
+            if (x[j-1] > x[j]):
+                conditionals += 1
+                x[j-1], x[j] = x[j], x[j-1]
+                assignments += 2
+    return (x, assignments, conditionals)
 
-    assert 1 == 1
-    return x
 
 def quicksort(x):
-    """
-    Describe how you are sorting `x`
-    """
+    global conditional
+    global assignment
+    conditional = 0
+    assignment = 0
+    quicksortRun(x, 0, len(x)-1),
+    return x, conditional, assignment
 
-    assert 1 == 1
-    return
+def quicksortRun(L, lo, hi):
+    global conditional
+    global assignment
+    if lo < hi:
+        conditional += 1
+        p = partition(L, lo, hi)
+        assignment += 1
+        quicksortRun(L, lo, p-1)
+        quicksortRun(L, p+1, hi)
+    return L
 
+def partition(L,lo,hi):
+    global conditional
+    global assignment
+    pivot = L[hi] 
+    assignment += 1
+    i = lo-1 
+    assignment += 1        
+    for j in range(lo, hi): 
+        assignment += 1
+        if (L[j] <= pivot): 
+            conditional += 1
+            i += 1
+            assignment += 1
+            L[i],L[j] = L[j],L[i] 
+            assignment += 2
+    L[i+1],L[hi] = L[hi],L[i+1] 
+    assignment += 2
+    newPivot = i+1
+    assignment += 1
+    return newPivot
--- test/test_algs.py
+++ test/test_algs.py
@@ -15,20 +15,66 @@ def test_pointless_sort():
     assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
 
 def test_bubblesort():
-    # Actually test bubblesort here. It might be useful to think about
-    # some edge cases for your code, where it might fail. Some things to
-    # think about: (1) does your code handle 0-element arrays without
-    # failing, (2) does your code handle characters?
 
-    x = np.array([1,2,4,0,1])
+    # Negatives, even
+    (sortedL, con, asg) = algs.bubblesort(np.array([-2,2,-51,0]))
+    assert(np.array_equal(sortedL, np.array([-51,-2,0,2])))
+
+    # Odd
+    (sortedL, con, asg) = algs.bubblesort(np.array([-2,2,-51]))
+    assert(np.array_equal(sortedL, np.array([-51,-2,2])))
+
+    # Duplicates
+    (sortedL, con, asg) = algs.bubblesort(np.array([1,1,1]))
+    assert(np.array_equal(sortedL, np.array([1,1,1])))
     
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
-    algs.bubblesort(x)
+    # Empty
+    (sortedL, con, asg) = algs.bubblesort(np.array([]))
+    assert(np.array_equal(sortedL, np.array([])))
+
+    # Characters
+    (sortedL, con, asg) = algs.bubblesort(np.array(['a','d','b','c','a']))
+    assert(np.array_equal(sortedL, np.array(['a','a', 'b','c','d'])))
+
+    # Strings
+    (sortedL, con, asg) = algs.bubblesort(np.array(['cca','aac','aab','aca','aaa']))
+    assert(np.array_equal(sortedL, np.array(['aaa','aab','aac','aca','cca'])))
+
+    # Sorted
+    (sortedL, con, asg) = algs.bubblesort(np.array([1,2,3]))
+    assert(np.array_equal(sortedL, np.array([1,2,3])))
+
+    # Reverse Sorted
+    (sortedL, con, asg) = algs.bubblesort(np.array([3,2,1]))
+    assert(np.array_equal(sortedL, np.array([1,2,3])))
+
 
 def test_quicksort():
 
-    x = np.array([1,2,4,0,1])
-    # for now, just attempt to call the quicksort function, should
-    # actually check output
-    algs.quicksort(x)
+    # Negatives, even
+    (sortedL, con, asg) = algs.quicksort(np.array([-2,2,-51,0]))
+    assert(np.array_equal(sortedL, np.array([-51,-2,0,2])))
+
+    # Odd
+    (sortedL, con, asg) = algs.quicksort(np.array([-2,2,-51]))
+    assert(np.array_equal(sortedL, np.array([-51,-2,2])))
+
+    # Duplicates
+    (sortedL, con, asg) = algs.quicksort(np.array([1,1,1]))
+    assert(np.array_equal(sortedL, np.array([1,1,1])))
+    
+    # Empty
+    (sortedL, con, asg) = algs.quicksort(np.array([]))
+    assert(np.array_equal(sortedL, np.array([])))
+
+    # Characters
+    (sortedL, con, asg) = algs.quicksort(np.array(['a','d','b','c','a']))
+    assert(np.array_equal(sortedL, np.array(['a','a', 'b','c','d'])))
+
+    # Sorted
+    (sortedL, con, asg) = algs.quicksort(np.array([1,2,3]))
+    assert(np.array_equal(sortedL, np.array([1,2,3])))
+
+    # Reverse Sorted
+    (sortedL, con, asg) = algs.quicksort(np.array([3,2,1]))
+    assert(np.array_equal(sortedL, np.array([1,2,3])))
